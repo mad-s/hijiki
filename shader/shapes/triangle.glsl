97239fbedf77e783ac69f85969c6ae1b30dcb7e9
mad-s <13462849+mad-s@users.noreply.github.com>
2019-12-17 17:11:10 +0100

    fix

---
+++ shader/shapes/triangle.glsl
@@ -0,0 +1,48 @@
+struct Vertex {
+	vec4 pos_u;
+	vec4 norm_v;
+};
+
+bool intersectTriangle(Ray ray, Vertex a, Vertex b, Vertex c, inout Intersection its) {
+	vec3 ab = b.pos_u.xyz - a.pos_u.xyz;
+	vec3 ac = c.pos_u.xyz - a.pos_u.xyz;
+
+	vec3 n = cross(ab, ac);
+	vec3 ro = ray.origin - a.pos_u.xyz;
+	vec3 q = cross(ro, ray.direction);
+	float d = 1./dot(ray.direction, n);
+	float u = d*dot(-q, ac);
+	float v = d*dot( q, ab);
+
+	if (u<0. || v<0. || u+v > 1.) {
+		return false;
+	}
+
+	float t = d*dot(-n, ro);
+	if (ray.tMin <= t && t <= ray.tMax) {
+		its.t = t;
+		its.n = normalize(n);
+		return true;
+	}
+	return false;
+}
+
+
+void sampleTriangle(Vertex a, Vertex b, Vertex c, out ShapeQueryRecord sRec) {
+	vec3 ab = b.pos_u.xyz - a.pos_u.xyz;
+	vec3 ac = c.pos_u.xyz - a.pos_u.xyz;
+
+	vec3 n = cross(ab, ac);
+	float area = length(n)/2.;
+	n /= 2.*area;
+
+	vec3 lambda = randBarycentric();
+	sRec.n = n;
+	sRec.p =
+		a.pos_u.xyz * lambda[0] + 
+		b.pos_u.xyz * lambda[1] + 
+		c.pos_u.xyz * lambda[2];
+	sRec.pdf = 1.0 / area;
+}
+
+
