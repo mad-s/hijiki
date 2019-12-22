bae3d620810946d14a3edecc584f14505fdf9282
Nils Leuzinger <nilsl@student.ethz.ch>
2019-12-22 18:35:13 +0100

    Swap the portal materials order

--- shader/materials/portal.glsl
+++ shader/materials/portal.glsl
@@ -1,7 +1,7 @@
 struct PortalMaterial {
 	mat4 transform_from;
         mat4 transform_to;
-        vec3 origin_to;
-        float pad;
         vec3 origin_from;
+        float pad;
+        vec3 origin_to;
 };
